5018a323eed07f320fb3810c3d60c8ad36f8ce8a
Kaunaz <37254203+Kaunaz@users.noreply.github.com>
2022-09-05 16:28:46 -0300

    Avances mods example 5/9 @lds

---
+++ Central/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	pb "github.com/Kendovvul/Ejemplo/Proto"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"google.golang.org/grpc"
+)
+
+func main () {
+	qName := "Emergencias" //Nombre de la cola
+	hostQ := "localhost"  //Host de RabbitMQ 172.17.0.1
+	hostS := "localhost" //Host de un Laboratorio
+	connQ, err := amqp.Dial("amqp://guest:guest@"+hostQ+":5672") //Conexion con RabbitMQ
+
+	if err != nil {log.Fatal(err)}
+	defer connQ.Close()
+
+	ch, err := connQ.Channel()
+	if err != nil{log.Fatal(err)}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(qName, false, false, false, false, nil) //Se crea la cola en RabbitMQ
+	if err != nil {log.Fatal(err)}
+
+	fmt.Println(q)
+
+	merc := 2
+
+	fmt.Println("Esperando Emergencias")
+	chDelivery, err := ch.Consume(qName, "", true, false, false, false, nil) //obtiene la cola de RabbitMQ
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	for delivery := range chDelivery {
+		if merc > 0 {
+			port := ":50051"  //puerto de la conexion con el laboratorio
+			fmt.Println("Pedido de ayuda de " + string(delivery.Body)) //obtiene el primer mensaje de la cola
+			merc--
+			connS, err := grpc.Dial(hostS + port, grpc.WithInsecure()) //crea la conexion sincrona con el laboratorio
+
+			if err != nil {
+				panic("No se pudo conectar con el servidor" + err.Error())
+			}
+		
+			defer connS.Close()
+		
+
+			serviceCliente := pb.NewMessageServiceClient(connS)
+
+			res, err := serviceCliente.Intercambio(context.Background(), 
+				&pb.Message{
+					Body: "Equipo listo?",
+					Esc: strconv.Itoa(merc),
+				})
+		
+			if err != nil {
+				panic("No se puede crear el mensaje " + err.Error())
+			}
+			fmt.Println(res.Body) 
+			for {
+				//envia el mensaje al laboratorio
+				res, err := serviceCliente.Intercambio(context.Background(), 
+					&pb.Message{
+						Body: "Equipo listo?",
+					})
+		
+				if err != nil {
+					panic("No se puede crear el mensaje " + err.Error())
+				}
+
+				fmt.Println(res.Body) //respuesta del laboratorio
+				if res.Body == "SI"{
+					merc++
+					break
+				}
+				// TODO Aqui agregar if resp = SI mercenarios vuelven -> Logica fin conn 
+				time.Sleep(5 * time.Second) //espera de 5 segundos
+			}
+		}
+		
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Tarea1-Grupo22
+
+# Ejemplo
+
+## Version local o una sola VM
+
+### Compilar proto
+    protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative Proto/message.proto
+
+### GO
+
+Ejecutar cada uno en una consola distinta.
+
+Para la central.
+    
+    go run Central/main.go
+
+Para el Laboratorio
+
+    go run Laboratorio/main.go
