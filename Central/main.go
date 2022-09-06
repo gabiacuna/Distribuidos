e0d236c108b85b4bf8f5923be6f9fcabe89471e8
Kaunaz <37254203+Kaunaz@users.noreply.github.com>
2022-09-06 14:45:12 -0300

    Update central

--- Central/main.go
+++ Central/main.go
@@ -7,32 +7,26 @@ import (
 	"strconv"
 	"time"
 
-	pb "github.com/Sistemas-Distribuidos-2022-2/Tarea1-Grupo22/Proto"
+	pb "github.com/Kendovvul/Ejemplo/Proto"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"google.golang.org/grpc"
 )
 
-func main() {
-	qName := "Emergencias"                                           //Nombre de la cola
-	hostQ := "localhost"                                             //Host de RabbitMQ 172.17.0.1
-	hostS := "localhost"                                             //Host de un Laboratorio
-	connQ, err := amqp.Dial("amqp://guest:guest@" + hostQ + ":5672") //Conexion con RabbitMQ
+func main () {
+	qName := "Emergencias" //Nombre de la cola
+	hostQ := "localhost"  //Host de RabbitMQ 172.17.0.1
+	hostS := "localhost" //Host de un Laboratorio
+	connQ, err := amqp.Dial("amqp://guest:guest@"+hostQ+":5672") //Conexion con RabbitMQ
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	if err != nil {log.Fatal(err)}
 	defer connQ.Close()
 
 	ch, err := connQ.Channel()
-	if err != nil {
-		log.Fatal(err)
-	}
+	if err != nil{log.Fatal(err)}
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(qName, false, false, false, false, nil) //Se crea la cola en RabbitMQ
-	if err != nil {
-		log.Fatal(err)
-	}
+	if err != nil {log.Fatal(err)}
 
 	fmt.Println(q)
 
@@ -43,53 +37,55 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	
 	for delivery := range chDelivery {
 		if merc > 0 {
-			port := ":50051"                                           //puerto de la conexion con el laboratorio
+			port := ":50051"  //puerto de la conexion con el laboratorio
 			fmt.Println("Pedido de ayuda de " + string(delivery.Body)) //obtiene el primer mensaje de la cola
 			merc--
-			connS, err := grpc.Dial(hostS+port, grpc.WithInsecure()) //crea la conexion sincrona con el laboratorio
+			connS, err := grpc.Dial(hostS + port, grpc.WithInsecure()) //crea la conexion sincrona con el laboratorio
 
 			if err != nil {
 				panic("No se pudo conectar con el servidor" + err.Error())
 			}
-
+		
 			defer connS.Close()
+		
 
 			serviceCliente := pb.NewMessageServiceClient(connS)
 
-			res, err := serviceCliente.Intercambio(context.Background(),
+			res, err := serviceCliente.Intercambio(context.Background(), 
 				&pb.Message{
 					Body: "Equipo listo?",
-					Esc:  strconv.Itoa(merc),
+					Esc: strconv.Itoa(merc),
 				})
-
+		
 			if err != nil {
 				panic("No se puede crear el mensaje " + err.Error())
 			}
-			fmt.Println(res.Body)
+			fmt.Println(res.Body) 
 			for {
 				//envia el mensaje al laboratorio
-				res, err := serviceCliente.Intercambio(context.Background(),
+				res, err := serviceCliente.Intercambio(context.Background(), 
 					&pb.Message{
 						Body: "Equipo listo?",
 					})
-
+		
 				if err != nil {
 					panic("No se puede crear el mensaje " + err.Error())
 				}
 
 				fmt.Println(res.Body) //respuesta del laboratorio
-				if res.Body == "SI" {
+				if res.Body == "SI"{
 					merc++
+					fmt.Println("Ha llegado a la central el equipo "+ res.Esc)
 					break
 				}
-				// TODO Aqui agregar if resp = SI mercenarios vuelven -> Logica fin conn
+				// TODO Aqui agregar if resp = SI mercenarios vuelven -> Logica fin conn 
 				time.Sleep(5 * time.Second) //espera de 5 segundos
 			}
 		}
-
+		
 	}
 
 }
