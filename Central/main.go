c2aec6d4f4e7758a7ea35c7bff513cda27615252
Gabriela Acuña <gacuna@trust.lat>
2022-09-07 23:31:22 -0300

    funcan los 4 clientes (labs) :D

--- Central/main.go
+++ Central/main.go
@@ -13,118 +13,175 @@ import (
 	"google.golang.org/grpc"
 )
 
-func main() {
+type server struct {
+	pb.UnimplementedMessageServiceServer
+}
 
-	qName := "Emergencias"                                           //Nombre de la cola
-	hostQ := "localhost"                                             //Host de RabbitMQ 172.17.0.1
-	hostS := "localhost"                                             //Host de un Laboratorio
-	connQ, err := amqp.Dial("amqp://guest:guest@" + hostQ + ":5672") //Conexion con RabbitMQ
+var merc int
 
+func custom_fatal(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func custom_panic(err error, msg string) {
+	if err != nil {
+		panic(msg + "\n error: " + err.Error())
+	}
+}
+
+func custom_IDLE(conn *grpc.ClientConn, err error) {
+	if conn.GetState().String() != "IDLE" {
+		panic("No se puede cerrar la conexion " + err.Error())
+	}
+}
+
+func log_consultas(file *os.File, lab_name string, n int) {
+	line := lab_name + ";" + strconv.Itoa(n) + "\n"
+
+	_, err := file.WriteString(line)
+
+	custom_panic(err, "No se pudo escribir en el archivo")
+}
+
+func health_check(serviceCliente pb.MessageServiceClient, esc string) *pb.Message {
+	res, err := serviceCliente.Intercambio(context.Background(),
+		&pb.Message{
+			Body: "Estallido resuelto?",
+			Esc:  esc,
+		})
+	custom_panic(err, "No se puede crear el mensaje")
+
+	return res
+}
+
+func stop_lab_conn(serviceCliente pb.MessageServiceClient, conn *grpc.ClientConn) {
+	_, err := serviceCliente.Intercambio(context.Background(),
+		&pb.Message{
+			Body: "STOP",
+		})
+	custom_IDLE(conn, err)
+
+}
+
+func check_on_lab(serviceCliente pb.MessageServiceClient, port string, lab string, file *os.File, connS *grpc.ClientConn) {
+	first_iter := true
+	n_cons := 0
+	for {
+		this_esc := ""
+		if first_iter {
+			this_esc = strconv.Itoa(merc) // para inf al lab sobre el escuadron mandado
+			merc--
+			first_iter = false
+		}
+		//envia el mensaje al laboratorio
+		res := health_check(serviceCliente, this_esc)
+
+		n_cons++
+
+		fmt.Println("Estallido resuelto?: " + res.Body) //respuesta del laboratorio
+		// fmt.Println("Estallido resuelto?: " + res.Body) //respuesta del laboratorio
+
+		if res.Body == "SI" {
+			// fmt.Println("Ha vuelto a la central el equipo " + this_esc)
+			fmt.Println("Ha vuelto a la central el equipo " + res.Esc + ", se ha cerrado la conexion con " + lab)
+
+			fmt.Println("---------------------------------------------\n")
+			merc++
+
+			stop_lab_conn(serviceCliente, connS)
+
+			log_consultas(file, lab, n_cons)
+
+			// time.Sleep(5 * time.Second) // espera igual si el equipo resuelve el estallido
+			return
+		}
+		time.Sleep(5 * time.Second) //espera de 5 segundos
+	}
+}
+
+func send_merc(port string, hostS string, lab_name string, file *os.File) {
+	fmt.Println("\n---------------------------------------------")               //puerto de la conexion con el laboratorio
+	fmt.Println("Pedido de ayuda de " + lab_name + " [Mensaje asincrono leido]") //obtiene el primer mensaje de la cola
+	fmt.Println("Enviando equipo " + strconv.Itoa(merc) + " a " + lab_name)
+
+	conn, err := grpc.Dial(hostS+port, grpc.WithInsecure()) //crea la conexion sincrona con el laboratorio
+
+	custom_panic(err, "No se pudo conectar con el servidor")
+
+	defer conn.Close()
+
+	serviceCliente := pb.NewMessageServiceClient(conn)
+
+	check_on_lab(serviceCliente, port, lab_name, file, conn)
+
+	fmt.Println("LLego al retuuuurn")
+	return
+}
+
+func main() {
+	qName := "Emergencias" //Nombre de la cola
+	hostQ := "localhost"   //Host de RabbitMQ 172.17.0.1
+	hostS := "localhost"   //Host de un Laboratorio
+
+	port_lab1 := ":50051"
+	port_lab2 := ":50052"
+	port_lab3 := ":50053"
+	port_lab4 := ":50054"
+
+	name_lab1 := "Laboratorio Renca"
+	name_lab2 := "Laboratorio Pohang"
+	name_lab3 := "Laboratorio Kampala"
+	name_lab4 := "Laboratorio Pripyat"
+
+	connQ, err := amqp.Dial("amqp://guest:guest@" + hostQ + ":5672") //Conexion con RabbitMQ
+	custom_fatal(err)
 	defer connQ.Close()
 
 	ch, err := connQ.Channel()
-	if err != nil {
-		log.Fatal(err)
-	}
+	custom_fatal(err)
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(qName, false, false, false, false, nil) //Se crea la cola en RabbitMQ
-	if err != nil {
-		log.Fatal(err)
-	}
+	custom_fatal(err)
 
 	fmt.Println(q)
 
-	//Archivo SOLICITUDES.txt para log
 	file, err := os.Create("SOLICITUDES.txt") //NombreLab;CantidadDeConsultas
 	if err != nil {
 		log.Fatal("No se pudo crear el archivo SOLICITUDES.txt: " + err.Error())
 	}
+
 	defer file.Close()
 
-	merc := 2
-	n_cons := 0
+	merc = 2
 
 	fmt.Println("Esperando Emergencias")
-	chDelivery, err := ch.Consume(qName, "", true, false, false, false, nil) //obtiene la cola de RabbitMQ
-	if err != nil {
-		log.Fatal(err)
-	}
+	chDelivery, err := ch.Consume(qName, "", false, false, false, false, nil) //obtiene la cola de RabbitMQ
+	custom_fatal(err)
 
 	for delivery := range chDelivery {
-		n_cons = 0
 		if merc > 0 {
-			port := ":50051"
+			port := ""
+			delivery.Ack(false) // only ACK this msg
+
 			lab := string(delivery.Body)
-			fmt.Println("\n---------------------------------------------")          //puerto de la conexion con el laboratorio
-			fmt.Println("Pedido de ayuda de " + lab + " [Mensaje asincrono leido]") //obtiene el primer mensaje de la cola
-			merc--
-			fmt.Println("Enviando equipo " + strconv.Itoa(merc) + " a " + lab)
-			connS, err := grpc.Dial(hostS+port, grpc.WithInsecure()) //crea la conexion sincrona con el laboratorio
 
-			if err != nil {
-				panic("No se pudo conectar con el servidor" + err.Error())
+			if lab == name_lab1 {
+				port = port_lab1
+			} else if lab == name_lab2 {
+				port = port_lab2
+			} else if lab == name_lab3 {
+				port = port_lab3
+			} else if lab == name_lab4 {
+				port = port_lab4
 			}
 
-			defer connS.Close()
-
-			serviceCliente := pb.NewMessageServiceClient(connS)
-
-			first_iter := true
-			for {
-				this_esc := ""
-				if first_iter {
-					this_esc = strconv.Itoa(merc) // para inf al lab sobre el escuadron mandado
-					first_iter = false
-				}
-				//envia el mensaje al laboratorio
-				res, err := serviceCliente.Intercambio(context.Background(),
-					&pb.Message{
-						Body: "Estallido resuelto?",
-						Esc:  this_esc,
-					})
-
-				n_cons++
-
-				if err != nil {
-					panic("No se puede crear el mensaje " + err.Error())
-				}
-
-				fmt.Println("Estallido resuelto?: " + res.Body) //respuesta del laboratorio
-				if res.Body == "SI" {
-					// fmt.Println("Ha vuelto a la central el equipo " + this_esc)
-					fmt.Println("Ha vuelto a la central el equipo " + res.Esc + ", se ha cerrado la conexion con " + string(delivery.Body))
-
-					fmt.Println("---------------------------------------------\n")
-					merc++
-
-					_, err := serviceCliente.Intercambio(context.Background(),
-						&pb.Message{
-							Body: "STOP",
-						})
-
-					if connS.GetState().String() != "IDLE" {
-						panic("No se puede cerrar la conexion " + err.Error())
-					}
-
-					line := lab + ";" + strconv.Itoa(n_cons) + "\n"
-
-					_, err = file.WriteString(line)
-
-					if err != nil {
-						log.Fatal("No se pudo escribir en el archivo")
-					}
-
-					// time.Sleep(5 * time.Second) // espera igual si el equipo resuelve el estallido
-					break
-				}
-				time.Sleep(5 * time.Second) //espera de 5 segundos
-
-			}
-			// connS.Close()
+			fmt.Println("\n------\n" + port + "\n------\n" + lab)
+			go send_merc(port, hostS, lab, file)
 		}
+		time.Sleep(5 * time.Second) //espera de 5 segundos
+
 	}
 }
---
+++ Laboratorio_2/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"time"
+
+	pb "github.com/Sistemas-Distribuidos-2022-2/Tarea1-Grupo22/Proto"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedMessageServiceServer
+}
+
+var this_esc string
+var resolved bool
+var serv *grpc.Server
+var port_central string
+
+func (s *server) Intercambio(ctx context.Context, msg *pb.Message) (*pb.Message, error) {
+
+	if msg.Esc != "" {
+		fmt.Println("El equipo " + msg.Esc + " ha llegado a contener el estallido!")
+		this_esc = msg.Esc
+	}
+
+	if msg.Body == "STOP" {
+		serv.Stop()
+		return &pb.Message{Body: "", Esc: this_esc}, nil
+	}
+	fmt.Println(msg.Body)
+
+	// TODO probabilidad de contencion
+	if rand.Intn(100) < 60 {
+		resolved = true
+		fmt.Println("Si!")
+		fmt.Println("Devolviendo escuadron " + this_esc)
+		fmt.Println("---------------------------------------------\n")
+		return &pb.Message{Body: "SI", Esc: this_esc}, nil
+	} else {
+		fmt.Println("No :c")
+		return &pb.Message{Body: "NO"}, nil
+	}
+}
+
+func main() {
+	LabName := "Laboratorio Pohang"                                  //nombre del laboratorio
+	qName := "Emergencias"                                           //nombre de la cola
+	hostQ := "localhost"                                             //ip del servidor de RabbitMQ 172.17.0.1
+	connQ, err := amqp.Dial("amqp://guest:guest@" + hostQ + ":5672") //conexion con RabbitMQ
+	rand.Seed(time.Now().UnixNano())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer connQ.Close()
+
+	ch, err := connQ.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+	fmt.Println("Estamos en " + LabName)
+
+	for {
+		resolved = false
+		for {
+			fmt.Println("Analizando estado del Laboratorio...")
+			if rand.Intn(100) < 80 {
+				fmt.Println("\n---------------------------------------------")
+				fmt.Println("Ha ocurrido un estallido en " + LabName + "! Mandando SOS a Central")
+				//Mensaje enviado a la cola de RabbitMQ (Llamado de emergencia)
+				err = ch.Publish("", qName, false, false,
+					amqp.Publishing{
+						Headers:     nil,
+						ContentType: "text/plain",
+						Body:        []byte(LabName), //Contenido del mensaje
+					})
+
+				if err != nil {
+					log.Fatal(err)
+				}
+				break
+				// fmt.Println(LabName)
+			} else {
+				fmt.Println("\nNo hay estallido en " + LabName + "\n")
+			}
+			time.Sleep(5 * time.Second)
+		}
+
+		listener, err := net.Listen("tcp", ":50052") //conexion sincrona
+		if err != nil {
+			panic("La conexion no se pudo crear" + err.Error())
+		}
+
+		serv = grpc.NewServer()
+		// fmt.Println("New sever")
+
+		// for {
+		pb.RegisterMessageServiceServer(serv, &server{})
+		// fmt.Println("service info")
+		// fmt.Println(serv.GetServiceInfo())
+		if err = serv.Serve(listener); err != nil {
+			panic("El server no se pudo iniciar" + err.Error())
+		}
+	}
+}
